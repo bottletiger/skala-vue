6c43105fbce6baefdc5575ef8c3b2466d6fef4e0
zzl-hyun <kimgihyun877@gmail.com>
2026-07-31 14:05:16 +0900

    Bonobono Cursor

---
+++ src/components/basic/SampleOne.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+let normalCount = 0;
+const vueCount = ref(754619123751923);
+
+</script>
+
+<template>
+<div>
+    <h3>{{ normalCount }}</h3>
+    <button @click="normalCount++">normalCount</button>
+    <h3>{{ vueCount }}</h3>
+    <button @click="vueCount++">vueCount</button>
+</div>
+
+</template>
