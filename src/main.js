8a81aac2d576cd106f7acaaf93916b3a3df956ff
강병호 <medit.kang@gmail.com>
2026-08-13 10:26:13 +0900

    Vue Devtools

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import devtools from '@vue/devtools'
+if (import.meta.env.PROD) {
+  devtools.connect(/* standalone devtools options */)
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
